a1f9f37f57b048bd1571aaf2fe2298d813d7abde
RuiHamada <rui.hamada@gmail.com>
2025-08-18 17:16:41 +0900

    chore: modify terraform format

--- terraform/env/main.tf
+++ terraform/env/main.tf
@@ -21,26 +21,26 @@ module "network" {
 module "cloudrun" {
   source = "../modules/cloudrun"
 
-  project_id                    = var.project_id
-  region                        = var.region
-  suffix                        = var.suffix
-  backlog_webhook_image_tag     = var.backlog_webhook_image_tag
-  backlog_webhook_secret_token  = var.backlog_webhook_secret_token
+  project_id                   = var.project_id
+  region                       = var.region
+  suffix                       = var.suffix
+  backlog_webhook_image_tag    = var.backlog_webhook_image_tag
+  backlog_webhook_secret_token = var.backlog_webhook_secret_token
 }
 
 # Load Balancer Module
 module "loadbalancer" {
   source = "../modules/loadbalancer"
 
-  project_id                    = var.project_id
-  region                        = var.region
-  suffix                        = var.suffix
-  domain_name                   = var.domain_name
-  backend_service_id            = module.network.backend_service_id
-  certificate_map_id            = module.network.certificate_map_id
-  lb_ip_address                 = var.lb_ip_address
-  backlog_webhook_service_name  = module.cloudrun.backlog_webhook_service_name
-  webhook_path                  = var.webhook_path
+  project_id                   = var.project_id
+  region                       = var.region
+  suffix                       = var.suffix
+  domain_name                  = var.domain_name
+  backend_service_id           = module.network.backend_service_id
+  certificate_map_id           = module.network.certificate_map_id
+  lb_ip_address                = var.lb_ip_address
+  backlog_webhook_service_name = module.cloudrun.backlog_webhook_service_name
+  webhook_path                 = var.webhook_path
 
   depends_on = [
     module.network,
